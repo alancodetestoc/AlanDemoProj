82d0fcdc8bcf61e5133e0b0cc29708026297dab6
Ivan Dcosta <idc1407@hotmail.com>
2022-09-08 23:03:03 +0530

    Add project files.

---
+++ AlanDemoProj/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace AlanDemoProj.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string? Name { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(string name)
+        {
+            Name = name;
+        }
+
+        public IActionResult OnPostShow()
+        {
+            return RedirectToPage("Hello", new { name = Name });
+        }
+
+        public IActionResult OnPostClear()
+        {
+            return RedirectToPage("Hello");
+        }
+    }
+}
